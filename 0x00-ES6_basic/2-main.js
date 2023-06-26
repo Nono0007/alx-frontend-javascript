6a4aced5c9244ce1913e2b8d6ff14475fbb135e8
Nono0007 <noreenmagaya24@gmail.com>
2023-06-26 22:39:42 +0000

    2-arrow.js

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow.js';
+
+const const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
